31b5c5f58be1fad199de564a7b52aec87680e2b6
Luis Sevilla <luissevilla@Luiss-MacBook-Air.local>
2026-01-06 08:52:04 -0600

    mongodb firtssteps

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FeatureModule } from './features/feature.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [FeatureModule],
+  imports: [
+    FeatureModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/films-db'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
