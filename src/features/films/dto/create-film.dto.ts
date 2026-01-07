b5ac45a85495c4192eb4791dd2335a1e0be521d4
Luis Sevilla <luissevilla@Luiss-MacBook-Air.local>
2026-01-07 07:44:07 -0600

    adding trnasformations

--- src/features/films/dto/create-film.dto.ts
+++ src/features/films/dto/create-film.dto.ts
@@ -1,3 +1,4 @@
+import { Transform } from 'class-transformer';
 import {
   IsString,
   IsNotEmpty,
@@ -8,12 +9,19 @@ import {
   IsInt,
   Min,
   MaxLength,
+  Max,
+  ArrayMaxSize,
 } from 'class-validator';
+
+const yearSpace = 2;
+const MAX_YEAR_LAUNCH = new Date().getFullYear() + yearSpace;
+const MAX_KEYWORDS = 5;
 
 export class CreateFilmDto {
   @IsString()
   @IsNotEmpty()
   @MaxLength(150)
+  @Transform(({ value }) => value.trim().toUpperCase())
   name: string;
 
   @IsString()
@@ -23,14 +31,19 @@ export class CreateFilmDto {
 
   @IsInt()
   @Min(1888) // primera película de la historia 😉
+  @Max(MAX_YEAR_LAUNCH)
   yearLaunch: number;
 
   @IsUrl()
   imageUrl: string;
 
   @IsArray()
+  @ArrayMaxSize(MAX_KEYWORDS)
   @IsString({ each: true })
   @IsNotEmpty({ each: true })
+  @Transform(({ value }) =>
+    value.map((keyword: string) => keyword.trim().toUpperCase()),
+  )
   keywords: string[];
 
   @IsInt()
@@ -49,6 +62,9 @@ export class CreateFilmDto {
   @IsArray()
   @IsString({ each: true })
   @IsNotEmpty({ each: true })
+  @Transform(({ value }) =>
+    value.map((genre: string) => genre.trim().toUpperCase()),
+  )
   genres: string[];
 
   @IsString()
